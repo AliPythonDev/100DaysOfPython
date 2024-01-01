d15095da9bdb8023a9845f5ff28f82a3cc41d6b7
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-01-01 16:40:30 +0500

    Update and rename Day-2 Life-in-Weeks.py to 02.Life_in_Weeks.py

---
+++ Day-2/02.Life_in_Weeks.py
@@ -0,0 +1,6 @@
+#Life in Weeks Project
+#Blog Link https://waitbutwhy.com/2014/05/life-weeks.html
+age = input("Enter your Age: ")
+years = 90 - int(age)
+weeks = years * 52
+print(f"You have {weeks} weeks left.")
--- Day-2/Day-2 Life-in-Weeks.py
+++
@@ -1,4 +0,0 @@
-age = input("Enter your Age: ")
-years = 90 - int(age)
-weeks = years * 52
-print(f"You have {weeks} weeks left.")
