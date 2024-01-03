024d72ecc6cfee03424ae9ceb2e71fe994d84648
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-01-03 17:54:53 +0500

    Add files via upload

---
+++ Day-3/01.Else_and_Conditional_Operators.py
@@ -0,0 +1,6 @@
+print("Welcome to the Roller Coaster!")
+height = int(input("What is your height in cm? "))
+if height >= 120:
+    print("You can ride the Roller Coaster")
+else:
+    print("Sorry you have to grow taller before you can ride.")
