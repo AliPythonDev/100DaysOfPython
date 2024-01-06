eb59487dfac4ff302a40c8b3c95b5253fe852a16
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-01-06 11:50:33 +0500

    Create 08.Roller_Coaster_Ticket_Generator_3.0.py

---
+++ Day-3/08.Roller_Coaster_Ticket_Generator_3.0.py
@@ -0,0 +1,24 @@
+print("Welcome to the Roller Coaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
+if height >= 120:
+    print("You can ride the Roller Coaster")
+    age = int(input("What is your Age? "))
+    if age < 12:
+        bill = 5
+        print("Child tickets are $5.")
+    elif age <= 18:
+        bill = 7
+        print("Youth tickets are $7.")
+    elif age >= 45 and age <= 55:
+        print("Everything is going to be ok. Have a free ride on us!")
+    else:
+        bill = 12
+        print("Adult tickets are $12.")
+
+    wants_photo = input("Do you want a Photo taken? Y or N. ")
+    if wants_photo == "Y":
+        bill += 3
+    print(f"Your final bill is ${bill}")
+else:
+    print("Sorry you have to grow taller before you can ride.")
