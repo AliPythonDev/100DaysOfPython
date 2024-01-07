9c11a5aaa76fec7131327430bc4326468fc71aeb
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-01-07 12:35:40 +0500

    Add files via upload

---
+++ Day-4/01.Love_Calculator_2.0.py
@@ -0,0 +1,10 @@
+import random
+
+randomInteger = random.randint(1,10)
+print(randomInteger)
+
+randomFloat = random.random() * 5
+print(randomFloat)
+
+love_score = random.randint(1, 100)
+print(f"Your love score is {love_score}")
